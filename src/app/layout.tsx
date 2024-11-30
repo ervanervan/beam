fc55f16a217b39bc489172c7deae863b88294c77
ervanervan <ervanervan040@gmail.com>
2024-11-30 21:21:47 +0700

    initial project beam

---
+++ src/app/layout.tsx
@@ -0,0 +1,32 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({
+  variable: "--font-inter",
+  subsets: ["latin"],
+  display: "swap",
+  axes: ["opsz"],
+});
+
+export const metadata: Metadata = {
+  title: "Beam - Modern Design Website Landing Page",
+  description: "Beam - Modern Design Website Landing Page",
+  // icons: {
+  //   icon: "/favicon.svg",
+  // },
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${inter.variable} font-sans antialiased`}>
+        {children}
+      </body>
+    </html>
+  );
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,16 +2,33 @@ import type { Config } from "tailwindcss";
 
 export default {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/sections/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
+      // container: {
+      //   center: true,
+      //   padding: {
+      //     DEFAULT: "1rem",
+      //     md: "2rem",
+      //     lg: "4rem",
+      //   },
+      // },
+      // fontFamily: {
+      //   sans: ["var(--font-inter)", "sans-serif"],
+      // },
+      // screens: {
+      //   sm: "375px",
+      //   md: "768px",
+      //   lg: "1200px",
+      // },
+      // colors: {
+      //   background: "var(--background)",
+      //   foreground: "var(--foreground)",
+      // },
     },
   },
   plugins: [],
