88a15214b16909b348621b7afa81732670187440
ervanervan <ervanervan040@gmail.com>
2024-12-01 13:34:47 +0700

    update: Navbar component

--- src/sections/Navbar.tsx
+++ src/sections/Navbar.tsx
@@ -13,23 +13,25 @@ export default function Navbar() {
   return (
     <header>
       <div className="container">
-        <div>
-          <Image
-            src={logoPrimary}
-            alt="Logo image"
-            className="w-[54px] h:auto"
-          />
+        <div className="grid grid-cols-3 items-center">
+          <div>
+            <Image
+              src={logoPrimary}
+              alt="Logo image"
+              className="w-[54px] h:auto"
+            />
+          </div>
+          <div className="lg:flex items-center justify-center hidden">
+            <nav className="flex gap-10">
+              {navLinks.map((link) => (
+                <a key={link.label} href={link.href}>
+                  {link.label}
+                </a>
+              ))}
+            </nav>
+          </div>
+          <div></div>
         </div>
-        <div className="lg:flex items-center justify-center hidden">
-          <nav className="">
-            {navLinks.map((link) => (
-              <a key={link.label} href={link.href}>
-                {link.label}
-              </a>
-            ))}
-          </nav>
-        </div>
-        <div></div>
       </div>
     </header>
   );
