9529942fdd7f6b78be3a99f15a1aa454bb165eb9
ervanervan <ervanervan040@gmail.com>
2024-12-01 14:20:50 +0700

    update: add font style in tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -17,14 +17,33 @@ export default {
           lg: "4rem",
         },
       },
+
       fontFamily: {
         sans: ["var(--font-poppins)", "sans-serif"],
       },
+
       screens: {
         sm: "375px",
         md: "768px",
         lg: "1200px",
       },
+
+      fontSize: {
+        title: ["4.5rem", "5rem"], // Title: 72px / 80px
+        h1: ["3rem", "3.25rem"], // Heading 1: 48px / 52px
+        h2: ["2.375rem", "3.125rem"], // Heading 2: 38px / 50px
+        h3: ["2rem", "2.875rem"], // Heading 3: 32px / 46px
+        h4: ["1.5rem", "2.5rem"], // Heading 4: 24px / 40px
+        h5: ["1.25rem", "2.125rem"], // Heading 5: 20px / 34px
+        h6: ["1.125rem", "1.875rem"], // Heading 6: 18px / 30px
+        nav: ["0.875rem", "1.25rem"], // Nav/Buttons: 14px / 20px
+        body1: ["1.125rem", "1.875rem"], // Body/1: 18px / 30px
+        body2: ["1rem", "1.875rem"], // Body/2: 16px / 30px
+        body3: ["0.875rem", "1.5rem"], // Body/3: 14px / 24px
+        body4: ["0.625rem", "1rem"], // Body/4: 10px / 16px
+        kategori: ["0.875rem", "1.5rem"], // Kategori: 14px / 24px
+      },
+
       colors: {
         text: {
           DEFAULT: "#000000",
