2798b2b0cee3bc28d0c242cc97f54b2a937435e6
ervanervan <ervanervan040@gmail.com>
2024-12-02 16:52:08 +0700

    update: color text secondary ini tailwind config

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Hero from "@/sections/Hero";
+import Misi from "@/sections/Misi";
 
 export default function Home() {
   return (
     <>
       <Hero />
+      <Misi />
     </>
   );
 }
---
+++ src/sections/Misi.tsx
@@ -0,0 +1,13 @@
+import Tag from "@/components/Tag";
+
+export default function Misi() {
+  return (
+    <section className="py-24">
+      <div className="container">
+        <div>
+          <Tag>Misi</Tag>
+        </div>
+      </div>
+    </section>
+  );
+}
