12ac57966d66440e31585d06d6b8701131c10bec
ervanervan <ervanervan040@gmail.com>
2024-12-03 14:46:13 +0700

    update: vehicle section

---
+++ src/sections/Vehicle.tsx
@@ -0,0 +1,12 @@
+import DashedBorderContainer from "@/components/DashedBorderContainer";
+import React from "react";
+
+export default function Vehicle() {
+  return (
+    <section>
+      <DashedBorderContainer>
+        <div>Vihicle</div>
+      </DashedBorderContainer>
+    </section>
+  );
+}
