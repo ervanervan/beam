65a65c2244311f8b3ce0c8130fa5260a9690470d
ervanervan <ervanervan040@gmail.com>
2024-12-05 16:07:34 +0700

    update: VehicleSafety and VehicleSafetyCard in city page

--- src/components/VehicleSafetyCard.tsx
+++ src/components/VehicleSafetyCard.tsx
@@ -1,5 +1,59 @@
 import React from "react";
+import Tag from "./Tag";
+import Image, { StaticImageData } from "next/image";
 
-export default function VehicleSafetyCard() {
-  return <div className="w-full bg-red-200 h-[300px]"></div>;
+type VehicleSafetyCardProps = {
+  tag: string;
+  title: string;
+  description: string;
+  imageSrc: StaticImageData;
+  imageAlt: string;
+  textPosition?: "left" | "right";
+  imagePosition?: "left" | "right";
+};
+
+export default function VehicleSafetyCard({
+  tag,
+  title,
+  description,
+  imageSrc,
+  imageAlt,
+  textPosition = "left",
+  imagePosition = "right",
+}: VehicleSafetyCardProps) {
+  const isTextLeft = textPosition === "left";
+  const isImageLeft = imagePosition === "left";
+  return (
+    <div className="grid grid-cols-2 items-center gap-8">
+      {/* Konten Teks */}
+      <div className={`flex flex-col ${isTextLeft ? "order-1" : "order-2"}`}>
+        <div>
+          <Tag>{tag}</Tag>
+        </div>
+        <div>
+          <h3 className="mt-8 text-h3 text-text-blackPrimary font-medium">
+            {title}
+          </h3>
+          <p className="mt-3 text-text-blackSecondary text-body1">
+            {description}
+          </p>
+        </div>
+      </div>
+
+      {/* Konten Gambar */}
+      <div
+        className={`flex justify-end ${
+          isImageLeft ? "mr-auto order-1" : "order-2"
+        }`}
+      >
+        <div className="bg-gradientPrimary rounded-3xl w-full md:w-[33rem] p-16">
+          <Image
+            src={imageSrc}
+            alt={imageAlt}
+            className="w-full md:w-[24rem] h-auto"
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/sections/VehicleSafety.tsx
+++ src/sections/VehicleSafety.tsx
@@ -1,16 +1,61 @@
 import DashedBorderContainer from "@/components/DashedBorderContainer";
 import VehicleSafetyCard from "@/components/VehicleSafetyCard";
 import React from "react";
+import vehicleSafetyImage1 from "@/assets/images/vehicle-safety-card-image1.png";
+import vehicleSafetyImage2 from "@/assets/images/vehicle-safety-card-image2.png";
+import vehicleSafetyImage3 from "@/assets/images/vehicle-safety-card-image3.png";
+import vehicleSafetyImage4 from "@/assets/images/vehicle-safety-card-image4.png";
 
 export default function VehicleSafety() {
   return (
     <section>
       <DashedBorderContainer showBottom={false}>
         <div className="flex flex-col items-center gap-24">
-          <VehicleSafetyCard />
-          <VehicleSafetyCard />
-          <VehicleSafetyCard />
-          <VehicleSafetyCard />
+          <VehicleSafetyCard
+            tag="Keamanan kendaraan"
+            title="Keselamatan"
+            description="Bagi kami, keselamatan bukan hanya soal kendaraan yang berfungsi baik dan memenuhi standar pemerintah. Dengan perpaduan perangkat keras, perangkat lunak, dan edukasi, kami menghadirkan kendaraan mikromobilitas Beam yang paling aman — jaminan yang tak bisa diberikan oleh kendaraan pribadi."
+            imageSrc={vehicleSafetyImage1}
+            imageAlt="Vehicle safety image 1"
+            textPosition="left"
+            imagePosition="right"
+          />
+          <VehicleSafetyCard
+            tag="Keamanan kendaraan"
+            title="Jejak karbon"
+            description="Di Asia Pasifik, Beam adalah satu-satunya operator mobilitas listrik yang Bersertifikat Netral Iklim. Semua perjalanan dengan Beam tidak hanya 100% bertenaga listrik, tetapi juga telah disetujui memiliki berstatus netral karbon menyeluruh."
+            imageSrc={vehicleSafetyImage2}
+            imageAlt="Vehicle safety image 2"
+            textPosition="right"
+            imagePosition="left"
+          />
+          <VehicleSafetyCard
+            tag="Keamanan kendaraan"
+            title="Meningkatkan bisnis lokal"
+            description="Platform #BeamBooster kami memanfaatkan perpaduan teknologi GPS, notifikasi dalam aplikasi, dan insentif mitra untuk menstimulasi aktivitas ekonomi di masyarakat setempat Anda. Lebih baik lagi, pendaftarannya gratis untuk bisnis."
+            imageSrc={vehicleSafetyImage3}
+            imageAlt="Vehicle safety image 1"
+            textPosition="left"
+            imagePosition="right"
+          />
+          <VehicleSafetyCard
+            tag="Keamanan kendaraan"
+            title="Peluang kerja"
+            description="Program Beam Marshals kami menciptakan peluang kerja di bidang ekonomi. Mereka bertugas memelihara skuter, mengganti baterai, serta menyebarkan dan mengumpulkan skuter, memastikan pengendara dapat berkendara dengan aman dan nyaman kapan saja, di mana saja."
+            imageSrc={vehicleSafetyImage4}
+            imageAlt="Vehicle safety image 2"
+            textPosition="right"
+            imagePosition="left"
+          />
+          <VehicleSafetyCard
+            tag="Keamanan kendaraan"
+            title="Data untuk perencanaan kota"
+            description="Kami percaya data penting untuk memaksimalkan dampak positif di kota-kota mitra kami. Beam melacak dan menganalisis ribuan metrik untuk meningkatkan keamanan, memahami permintaan mikromobilitas, serta mendukung perencanaan kota dan pengembangan infrastruktur masa depan."
+            imageSrc={vehicleSafetyImage4}
+            imageAlt="Vehicle safety image 2"
+            textPosition="right"
+            imagePosition="left"
+          />
         </div>
       </DashedBorderContainer>
     </section>
