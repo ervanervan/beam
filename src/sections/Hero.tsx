801de9392ba8dc52166936c3a19e4ae0e2c7e55a
ervanervan <ervanervan040@gmail.com>
2024-12-05 11:57:36 +0700

    update: add button accessibility hero section

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -43,6 +43,39 @@ const ArrowRightIcon = () => (
   </svg>
 );
 
+const AccessibilityIcon = () => (
+  <svg
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22Z"
+      stroke="#1F1F1F"
+      strokeWidth="1.6"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <path
+      d="M7 9L12 10M12 10L17 9M12 10V13M12 13L10 18M12 13L14 18"
+      stroke="#1F1F1F"
+      strokeWidth="1.6"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <path
+      d="M12 7C11.7239 7 11.5 6.77614 11.5 6.5C11.5 6.22386 11.7239 6 12 6C12.2761 6 12.5 6.22386 12.5 6.5C12.5 6.77614 12.2761 7 12 7Z"
+      fill="#1F1F1F"
+      stroke="#1F1F1F"
+      strokeWidth="1.6"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+);
+
 export default function Hero() {
   return (
     <section className="xl:px-7 xl:pt-7">
@@ -55,17 +88,26 @@ export default function Hero() {
             Ubah masa depan mulai dari dirimu dengan{" "}
             <br className="hidden md:block" /> menggunakan beam sekarang.
           </p>
-          <div className="flex flex-col md:flex-row gap-3 mt-11">
-            <Button variant="secondary" icon={<MapPinIcon />}>
-              Lihat Garasi Beam
-            </Button>
-            <Button
-              variant="primary"
-              icon={<ArrowRightIcon />}
-              iconPosition="right"
-            >
-              Unduh Aplikasi
-            </Button>
+          <div className="flex justify-between w-full items-end">
+            <div className="flex flex-col md:flex-row gap-3 mt-11">
+              <Button variant="secondary" icon={<MapPinIcon />}>
+                Lihat Garasi Beam
+              </Button>
+              <Button
+                variant="primary"
+                icon={<ArrowRightIcon />}
+                iconPosition="right"
+              >
+                Unduh Aplikasi
+              </Button>
+            </div>
+            <div>
+              <Button
+                variant="secondary"
+                icon={<AccessibilityIcon />}
+                className="px-[10px]"
+              />
+            </div>
           </div>
         </div>
       </div>
