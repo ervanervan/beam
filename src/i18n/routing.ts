71941cb454f75bd48424e4c3d60966d5bdfd2433
ervanervan <ervanervan040@gmail.com>
2024-12-15 19:09:33 +0700

    update: default routing multi language

--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -3,10 +3,10 @@ import { createNavigation } from "next-intl/navigation";
 
 export const routing = defineRouting({
   // A list of all locales that are supported
-  locales: ["en", "id", "fr"],
+  locales: ["id", "en", "fr"],
 
   // Used when no locale matches
-  defaultLocale: "en",
+  defaultLocale: "id",
 });
 
 // Lightweight wrappers around Next.js' navigation APIs
