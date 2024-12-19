9dbe770e89cb332fb8a3f2d326d7f98b60e01efb
ervanervan <ervanervan040@gmail.com>
2024-12-19 09:47:56 +0700

    update: qrcode data multi language, data messages on download page

--- src/sections/QrCode.tsx
+++ src/sections/QrCode.tsx
@@ -5,14 +5,16 @@ import qrCodeAndroid from "../../public/assets/images/qr-code-android.svg";
 import qrCodeIos from "../../public/assets/images/qr-code-ios.svg";
 import AndroidIcon from "../../public/assets/icons/AndroidIcon";
 import IosIcon from "../../public/assets/icons/IosIcon";
+import { useTranslations } from "next-intl";
 
 export default function QrCode() {
+  const t = useTranslations("QrCode");
   return (
     <section>
       <DashedBorderContainer showBottom={false}>
         <div>
           <h4 className="text-h4 font-medium text-text-blackSecondary max-w-lg mb-6">
-            Atau bisa juga mengunduh aplikasi Beam via QR Code dibawah ini.
+            {t("title")}
           </h4>
         </div>
         <div className="container max-w-xs md:max-w-[44rem] lg:max-w-6xl border-y border-others-santasGray border-dashed"></div>
@@ -26,13 +28,11 @@ export default function QrCode() {
                   <AndroidIcon />
                 </div>
                 <span className="text-h5 text-text-blackPrimary font-medium">
-                  Android
+                  {t("android.title")}
                 </span>
               </div>
               <p className="mt-3 text-body2 text-text-blackSecondary">
-                Nikmati perjalanan tanpa hambatan bersama Beam. Temukan, buka
-                kunci, dan kendarai skuter di kotamu dengan aplikasi Android
-                kami.
+                {t("android.description")}
               </p>
             </div>
           </div>
@@ -40,7 +40,7 @@ export default function QrCode() {
           <div className="absolute h-72 mt-2 border-x border-others-santasGray border-dashed left-1/2 -translate-x-1/2 hidden lg:block"></div>
 
           <div className="p-3 flex flex-col md:flex-row md:items-center gap-5">
-            <Image src={qrCodeIos} alt="Qr code android" className="w-36" />
+            <Image src={qrCodeIos} alt="Qr code iOS" className="w-36" />
 
             <div className="flex flex-col">
               <div className="flex items-center gap-3">
@@ -48,12 +48,11 @@ export default function QrCode() {
                   <IosIcon />
                 </div>
                 <span className="text-h5 text-text-blackPrimary font-medium">
-                  iOS
+                  {t("ios.title")}
                 </span>
               </div>
               <p className="mt-3 text-body2 text-text-blackSecondary">
-                Nikmati perjalanan tanpa hambatan bersama Beam. Temukan, buka
-                kunci, dan kendarai skuter di kotamu dengan aplikasi iOS kami.
+                {t("ios.description")}
               </p>
             </div>
           </div>
